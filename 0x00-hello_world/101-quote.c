69f5f451a4cbc711657eb9308f7c21003da19213
MurugiMutegi <murugimutegi@gmail.com>
2023-06-18 01:31:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main (void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
